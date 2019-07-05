0d8c8f7e8f7ba12bdee7f97ab116bf3f4181358f
yahya <sankarrynjieb@gmai.com>
2019-07-05 14:14:20 +0200

    make image full width

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 console.log('ready!');
+// var img = document.createElement("img");
+// img.src = "img/eqp/"+this.apparel+"/"+this.facing+"_idle.png";
+// src = document.getElementById("gamediv");
+// src.appendChild(this.img)
